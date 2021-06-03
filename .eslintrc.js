43f05f3094681cac3bcd44cb71edf341b413f0e1
Devang <devang@oorjan.com>
2021-06-03 15:41:40 +0530

    master: added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true,
+        'es6': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly',
+    },
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true,
+        },
+        'ecmaVersion': 2018,
+        'sourceType': 'module',
+    },
+    'plugins': [
+        'react',
+        'react-hooks',
+        '@typescript-eslint',
+    ],
+    'rules': {
+        'prettier/prettier': 'off', // Turn off normal prettier
+        'indent': ['error', 4,],
+        'linebreak-style': ['error', 'unix',],
+        'quotes': 'off',
+        'semi': ['error', 'always',],
+        // ref: https://www.npmjs.com/package/eslint-plugin-react-hooks
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        'no-console': 2,
+    },
+};
