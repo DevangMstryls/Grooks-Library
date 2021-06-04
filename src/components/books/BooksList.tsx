c21019a8b6ff9e6b54fc997cd71d6a31a8b0fc88
Devang Mistry <idevangmistry1@gmail.com>
2021-06-05 02:34:30 +0530

    master: made the filtering functionality

--- src/components/books/BooksList.tsx
+++ src/components/books/BooksList.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {APP_STATE, BooksState} from "../../core/types/stateTypes";
 import {connect} from "react-redux";
 import BookCard from "./BookCard";
+import {Book} from "../../core/types/types";
 
 const mapStateToProps = (state: APP_STATE) => {
     return {
@@ -21,13 +22,70 @@ const BooksList = (props: Props) => {
         // dispatch,
     } = props;
 
-    const allBooks = books.data;
+    const allBooks: { [bookId: string]: Book } = books.data;
 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [waitForSearch, setWaitForSearch] = useState(false);
+    const [filtersApplied, setFiltersApplied] = useState(false);
+    const [filteredBooks, setFilteredBooks] = useState({});
+
+    const timeoutRef: any = useRef();
+
+    const doSearch = () => {
+        if (searchTerm.trim() === '') {
+            setFiltersApplied(false);
+            return;
+        }
+
+        setWaitForSearch(true);
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => {
+            filterBooks();
+            setFiltersApplied(true);
+            setWaitForSearch(false);
+        }, 1000);
+    };
+
+    const filterBooks = () => {
+        const filtered: { [bookId: string]: Book } = {};
+        Object.values(allBooks).forEach((book) => {
+            const searchable = `${book.name} ${book.description} ${book.author} ${book.publisher}`;
+            if (searchable.toLowerCase().includes(searchTerm.toLowerCase())) {
+                filtered[book.id] = book;
+            }
+        });
+        setFilteredBooks(filtered);
+        setFiltersApplied(true);
+    };
+
+    useEffect(() => {
+        filterBooks();
+    }, [searchTerm]);
 
     return (
         <div>
+            <div>
+                <input
+                    type="search"
+                    placeholder={'Search for books by name, description, author, publisher'}
+                    value={searchTerm}
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value);
+                        if (e.target.value.trim() === '') {
+                            setFiltersApplied(false);
+                        }
+                    }}
+                    onKeyUp={(e) => {
+                        if (e.key === 'Enter') {
+                            doSearch();
+                        }
+                    }}
+                />
+                <button onClick={() => doSearch()}>{waitForSearch ? 'Searching...' : 'Search'}</button>
+            </div>
+
             {
-                Object.keys(allBooks).map((bookId) => {
+                Object.keys(filtersApplied ? filteredBooks : allBooks).map((bookId) => {
                     const book = allBooks[bookId];
                     return (
                         <BookCard
@@ -36,6 +94,11 @@ const BooksList = (props: Props) => {
                         />
                     );
                 })
+            }
+
+            {
+                (filtersApplied && !Object.keys(filteredBooks).length) &&
+                <p>No book found for &quot;{searchTerm}&quot;</p>
             }
         </div>
     );
