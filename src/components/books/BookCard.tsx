217fe6fd697ec5e577182c4602a7bccf3d4e7edf
Devang Mistry <idevangmistry1@gmail.com>
2021-06-06 01:04:17 +0530

    master/styling: added icons and styled the book card component

--- src/components/books/BookCard.tsx
+++ src/components/books/BookCard.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
 import {Book} from "../../core/types/types";
 import "./../../styles/components/BookCard.scss";
+import {ChevronDownIcon, ChevronUpIcon, DeleteIcon, EditIcon} from "../icons";
+import {useDispatch} from "react-redux";
+import {ACTION_TYPES} from "../../core/constants";
 
 
 type Props = {
@@ -11,26 +14,66 @@ type Props = {
 const BookCard = (props: Props) => {
 
     const routerHistory = useHistory();
+    const [showMenu, setShowMenu] = useState(false);
+    const dispatch = useDispatch();
 
     const {
         book,
-        // dispatch,
     } = props;
 
     // TODO: remove these
     book['cover'] = 'https://images-na.ssl-images-amazon.com/images/I/5112YFsXIJL.jpg';
     book['price'] = 300;
 
-    const handleOnClick = (e: any) => {
-        if (e.target.nodeName !== 'A')
+    const handleOnClick = (e: any): void => {
+        if (!e.target.closest('.menu-open-btn') && !e.target.closest('.menu'))
             routerHistory.push(`/book/${book.id}`);
+    };
+
+    const handleDropdownClick = (): void => {
+        setShowMenu(!showMenu);
+    };
+
+    const handleEditClick = (): void => {
+        routerHistory.push(`/edit/${book.id}`);
+    };
+
+    const handleDeleteClick = (): void => {
+        if (confirm("Are you sure you want to delete this book?")) {
+            dispatch({
+                type: ACTION_TYPES.DELETE_BOOK,
+                payload: {
+                    id: book.id,
+                },
+            });
+        }
     };
 
     return (
         <div className="book-card" onClick={handleOnClick}>
-            <div>
+            <div className="pos-rel">
+                <button className="btn-icon menu-open-btn pos-abs" onClick={handleDropdownClick}>
+                    {showMenu ? <ChevronUpIcon/> : <ChevronDownIcon/>}
+                </button>
 
+                {
+                    showMenu &&
+                    <div className="menu pos-abs">
+                        <div onClick={handleEditClick} className="menu-item flex-row flex-align-items-center">
+                            <EditIcon/>
+                            <span>&nbsp;Edit</span>
+                        </div>
+                        <div
+                            onClick={handleDeleteClick}
+                            className="menu-item flex-row flex-align-items-center delete-item"
+                        >
+                            <DeleteIcon/>
+                            <span>&nbsp;Delete</span>
+                        </div>
+                    </div>
+                }
             </div>
+
             <div className="flex-row flex-align-flex-start">
                 <div className="book-cover flex-center pos-rel">
                     <img className="v-al-mdl" src={book.cover} alt={book.name}/>
@@ -42,7 +85,8 @@ const BookCard = (props: Props) => {
                     </div>
                     <div className="flex-row flex-justify-space-between flex-align-items-center">
                         <p className="txt-clr-gray-1 p2 m-0">₹&nbsp;{book.price}</p>
-                        <p className={`p4 m-0 ${book.availableStock < 10 ? 'txt-clr-error': 'txt-clr-gray-1'}`}>In Stock:&nbsp;{book.availableStock}</p>
+                        <p className={`p4 m-0 ${book.availableStock < 10 ? 'txt-clr-error' : 'txt-clr-gray-1'}`}>In
+                            Stock:&nbsp;{book.availableStock}</p>
                     </div>
                 </div>
             </div>
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-export const ChevronDownIcon = () => {
+type IconProps = {
+    height?: number,
+    width?: number,
+    color?: string,
+}
+
+export const ChevronDownIcon = (props: IconProps) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
             <path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/>
@@ -9,7 +15,7 @@ export const ChevronDownIcon = () => {
     );
 };
 
-export const ChevronUpIcon = () => {
+export const ChevronUpIcon = (props: IconProps) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
             <path d="M0 0h24v24H0V0z" fill="none"/>
@@ -18,7 +24,7 @@ export const ChevronUpIcon = () => {
     );
 };
 
-export const SearchIcon = () => {
+export const SearchIcon = (props: IconProps) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
             <path d="M0 0h24v24H0V0z" fill="none"/>
@@ -29,7 +35,7 @@ export const SearchIcon = () => {
 };
 
 
-export const AddIcon = () => {
+export const AddIcon = (props: IconProps) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
             <path d="M0 0h24v24H0V0z" fill="none"/>
@@ -39,11 +45,46 @@ export const AddIcon = () => {
 };
 
 
-export const DeleteIcon = () => {
+export const DeleteIcon = (props: IconProps) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
             <path d="M0 0h24v24H0V0z" fill="none"/>
             <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/>
+        </svg>
+    );
+};
+
+
+export const EditIcon = (props: IconProps) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+            <path d="M0 0h24v24H0V0z" fill="none"/>
+            <path
+                d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z"/>
+        </svg>
+    );
+};
+
+
+export const BookIcon = (props: IconProps) => {
+    const {
+        height = 24,
+        width = 24,
+        color = '#000000',
+    } = props;
+
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height={`${height}px`}
+            viewBox="0 0 24 24"
+            width={`${width}px`} fill={color}>
+            <g>
+                <rect fill="none" height={height} width={width} x="0"/>
+            </g>
+            <g>
+                <path
+                    d="M22.47,5.2C22,4.96,21.51,4.76,21,4.59v12.03C19.86,16.21,18.69,16,17.5,16c-1.9,0-3.78,0.54-5.5,1.58V5.48 C10.38,4.55,8.51,4,6.5,4C4.71,4,3.02,4.44,1.53,5.2C1.2,5.36,1,5.71,1,6.08v12.08c0,0.58,0.47,0.99,1,0.99 c0.16,0,0.32-0.04,0.48-0.12C3.69,18.4,5.05,18,6.5,18c2.07,0,3.98,0.82,5.5,2c1.52-1.18,3.43-2,5.5-2c1.45,0,2.81,0.4,4.02,1.04 c0.16,0.08,0.32,0.12,0.48,0.12c0.52,0,1-0.41,1-0.99V6.08C23,5.71,22.8,5.36,22.47,5.2z M10,16.62C8.86,16.21,7.69,16,6.5,16 c-1.19,0-2.36,0.21-3.5,0.62V6.71C4.11,6.24,5.28,6,6.5,6C7.7,6,8.89,6.25,10,6.72V16.62z M19,0.5l-5,5V15l5-4.5V0.5z"/>
+            </g>
         </svg>
     );
 };
