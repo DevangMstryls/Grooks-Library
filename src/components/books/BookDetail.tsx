097c0d5547cf4c4dccfe4bf04f5c9c6b8532403a
Devang Mistry <idevangmistry1@gmail.com>
2021-06-06 12:35:58 +0530

    master/styling: added options dropdown on add update book page and made associated changes

--- src/components/books/BookDetail.tsx
+++ src/components/books/BookDetail.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {APP_STATE, BooksState} from "../../core/types/stateTypes";
-import {connect} from "react-redux";
+import {connect, useDispatch} from "react-redux";
 import {useParams} from "react-router";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import "./../../styles/components/BookDetail.scss";
+import {ChevronDownIcon, ChevronUpIcon, DeleteIcon, EditIcon} from "../icons";
+import {ACTION_TYPES} from "../../core/constants";
 
 
 const mapStateToProps = (state: APP_STATE) => {
@@ -19,6 +21,9 @@ type Props = {
 const BookDetail = (props: Props) => {
 
     const params: any = useParams();
+    const routerHistory = useHistory();
+    const [showMenu, setShowMenu] = useState(false);
+    const dispatch = useDispatch();
 
     const {
         books,
@@ -26,6 +31,25 @@ const BookDetail = (props: Props) => {
     } = props;
 
     const book = books.data[params.id] || null;
+
+    const handleDropdownClick = (): void => {
+        setShowMenu(!showMenu);
+    };
+
+    const handleEditClick = (): void => {
+        routerHistory.push(`/edit/${book.id}`);
+    };
+
+    const handleDeleteClick = (): void => {
+        if (confirm("Are you sure you want to delete this book?")) {
+            dispatch({
+                type: ACTION_TYPES.DELETE_BOOK,
+                payload: {
+                    id: book.id,
+                },
+            });
+        }
+    };
 
     if (!book) {
         return (
@@ -39,13 +63,32 @@ const BookDetail = (props: Props) => {
                 <Link to={'/'}>&lt; Back</Link>
             </p>
 
-            <div className="book-details-head">
-                <div>
-                    <div className="book-cover-wrpr pos-rel">
-                        <img
-                            className="v-al-mdl"
-                            src={'https://images-na.ssl-images-amazon.com/images/I/5112YFsXIJL.jpg'}/>
+            <div className="book-details-head pos-rel">
+                <button className="btn-icon menu-open-btn pos-abs" onClick={handleDropdownClick}>
+                    {showMenu ? <ChevronUpIcon/> : <ChevronDownIcon/>}
+                </button>
+
+                {
+                    showMenu &&
+                    <div className="menu pos-abs">
+                        <div onClick={handleEditClick} className="menu-item flex-row flex-align-items-center cur-p">
+                            <EditIcon/>
+                            <span>&nbsp;Edit</span>
+                        </div>
+                        <div
+                            onClick={handleDeleteClick}
+                            className="menu-item flex-row flex-align-items-center delete-item cur-p"
+                        >
+                            <DeleteIcon/>
+                            <span>&nbsp;Delete</span>
+                        </div>
                     </div>
+                }
+
+                <div className="book-cover-wrpr flex-row flex-justify-center">
+                    <img
+                        className="v-al-mdl"
+                        src={'https://images-na.ssl-images-amazon.com/images/I/5112YFsXIJL.jpg'}/>
                 </div>
                 <div className="book-main-details flex-column flex-justify-space-between">
                     <div>
